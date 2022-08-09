2e44615ad2c23f25f255956fa31ae354fa6fa4b5
Soham Chakraborty <104936089+sohamc2001@users.noreply.github.com>
2022-08-10 01:48:43 +0530

    Initial commit

---
+++ src/components/Notes.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Notes() {
+  return (
+    <div className="note">
+      <h1>Devtown Bootcamp</h1>
+      <p>
+        React is a free and open-source front-end JavaScript library for
+        building user interfaces based on UI components. It is maintained by
+        Meta and a community of individual developers and companies.
+      </p>
+    </div>
+  );
+}
+
+export default Notes;
